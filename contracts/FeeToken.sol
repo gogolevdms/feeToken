f1ace393e407e11371a4865497b9a497ed1a8758
Dmitriy Gogolev <gogolevdms@gmail.com>
2022-01-15 11:45:01 +0300

    fix coding style

--- contracts/FeeToken.sol
+++ contracts/FeeToken.sol
@@ -2,61 +2,55 @@
 pragma solidity 0.8.11;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/utils/Context.sol";
 import '@openzeppelin/contracts/access/Ownable.sol';
 
 /**
  * @title FeeToken.
  */
-
 contract FeeToken is ERC20, Ownable {
-    uint public taxFee = 25;
+    uint public fee = 25;
     uint public denom = 10000;
 
     address public wallet;
 
-    event SetFee(uint _oldFee, uint _newFee);
-    event SetWallet(address _oldWallet, address _newWallet);
+    event NewFee(uint oldFee, uint newFee);
+    event NewWallet(address oldWallet, address newWallet);
 
     constructor(address account, string memory name_, string memory symbol_) ERC20(name_, symbol_) {
         _mint(account, 12884901889e18);
     }
 
-    /// @notice The function sets the new tax amount.
-    /// @dev Stores the unsigned int value in the state variable 'taxFee'.
-    /// @param _taxFee The new value to store.
+    /// @notice The function sets the new fee percent.
+    /// @param _newFee The fee value.
     /// @return The bool value.
-    function _setFee(uint _taxFee) external onlyOwner returns (bool) {
-        taxFee = _taxFee;
+    function _setFee(uint newFee_) external onlyOwner returns (bool) {
+        uint oldFee = fee;
+        fee = newFee_;
 
-        emit SetFee(taxFee, _taxFee);
+        emit NewFee(oldFee, fee);
 
         return true;
     }
 
     /// @notice The function sets a new wallet.
-    /// @dev Stores the address value in the state variable 'wallet'.
     /// @param _wallet The new value to store.
     /// @return The bool value.
-    function _setWallet(address _wallet) external onlyOwner returns (bool) {
-        wallet = _wallet;
+    function _setWallet(address newWallet_) external onlyOwner returns (bool) {
+        address oldWallet = wallet;
+        wallet = newWallet_;
 
-        emit SetWallet(wallet, _wallet);
+        emit NewWallet(oldWallet, wallet);
 
         return true;
     }
 
-    /// @notice The function transfers tokens.
+    /// @notice The function transfers tokens including fee.
     /// @param sender The address of the token sender.
     /// @param recipient Address of the token recipient.
     /// @param amount Number of tokens to transfer.
-    function _transfer(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) internal override {
-        uint256 fee = amount * taxFee / denom;
-        uint256 net = amount - fee;
+    function _transfer(address sender, address recipient, uint amount) internal override {
+        uint fee = amount * taxFee / denom;
+        uint net = amount - fee;
 
         super._transfer(sender, recipient, net);
         super._transfer(sender, wallet, fee);
